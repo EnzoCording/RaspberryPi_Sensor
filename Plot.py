6c6c228cd533d2cd89809e51196cc67320925c6e
Enzo Cording <enzocording@web.de>
2017-04-28 16:00:06 +0200

    Add files via upload

--- Plot.py
+++ Plot.py
@@ -1,27 +1,28 @@
+'''
+Author: Enzo Cording
+'''
+
 from datetime import datetime
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
-#import numpy as np
 import matplotlib.animation as animation
-#import pylab
 import os
-#import time
 
 x = []	
 y = []
 t = []
 h = []
 
-
-fig = plt.figure(figsize=(10,7.5))
+fig = plt.figure(figsize=(7,6.5))
 ax1 = fig.add_subplot(2, 1, 2)
 ax2 = ax1.twinx()
 ax3 = fig.add_subplot(2, 1, 1)
 ax4 = ax3.twinx()
-CMD = r'"pscp pi@tb-raspberry-01:/home/pi/RPi_HTP/BMP180/Python/document.csv C:\Users\cordinge\Desktop\RaspberryPi_Sensor-master"'
+fig.canvas.set_window_title('IR Chamber - Humidity and Temperature')
+
+CMD = r'"pscp -scp pi@fhlrasptemp.desy.de:/home/pi/pitemp/document.csv C:\Users\cordinge\Desktop\RaspberryPi_Sensor-master"'
 
 def plot(i):
- 
  
  readFile = open('document.csv', 'r')
  sepFile = readFile.read().split('\n')
@@ -34,23 +35,19 @@ def plot(i):
          xAndY = plotPair.split(',')
          time_string = xAndY[0]
          time_string1 = datetime.strptime(time_string, '%d/%m/%Y %H:%M:%S')
-         #print(time_string1)
-         #print (len(y))
+      
          t.append(time_string1)
          y.append(float(xAndY[1]))
          h.append(float(xAndY[2]))
-  	
-
+        
  avgtemp = sum(y)/len(y)
  avghumid = sum(h)/len(h)
  
-
  tentime = t[-60:]
  tentemp = y[-60:]
  tenhumid = h[-60:] 
 
  ax1 = fig.add_subplot(2, 1, 2)
-
 
  ax1.clear()
  ax1.plot(t, y, 'o', color = 'r')
@@ -58,16 +55,16 @@ def plot(i):
  ax1.set_ylabel('TEMPERATURE in C')
  ax1.set_xlabel('TIME')
  ax1.yaxis.label.set_color('red')
- #plt.xticks(rotation=80)
+ plt.xticks(rotation=20)
  ax1.grid()
-
+ ax1.tick_params(axis='y', colors='red')
 
  ax2.clear()
  ax2.plot(t, h, 'o', color = 'g') 
  ax2.set_yticks([4,12,20,28,36,44,52,60,68,76,84,92,100])
  ax2.set_ylabel('HUMIDITY in %')
  ax2.yaxis.label.set_color('green')
-
+ ax2.tick_params(axis='y', colors='green')
 
  ax3.clear()
  ax3.plot(tentime, tentemp, 'o', color = 'r')
@@ -79,19 +76,17 @@ def plot(i):
  ax3.set_title('Last Five Minutes: ' + 'Temperature: ' + "%.2f" % avgtentemp + '  ' + 'Humidity: '  + "%.2f" % avgtenhumid)
  ax3.set_xlabel('TIME')
  ax3.grid()
-
+ ax3.tick_params(axis='y', colors='red')
 
  ax4.clear()
  ax4.plot(tentime, tenhumid, 'o', color = 'g')
  ax4.set_yticks([16,18,20,22,24,26,28,30,32,34,36,38,40,42,44,46,48,50]) 
  ax4.set_ylabel('HUMIDITY in %')
  ax4.yaxis.label.set_color('green')
+ ax4.tick_params(axis='y', colors='green')
 
- 
  ax1.xaxis.set_major_formatter(mdates.DateFormatter('%d/%m' + ' - ' + '%H:%M:%S'))
-
  ax3.xaxis.set_major_formatter(mdates.DateFormatter('%H:%M:%S'))
- #fig.autofmt_xdate(rotation=45)
  
  plt.title('Average Temperature ' +  "%.2f" % avgtemp + '   ' + 'Average Humidity ' + "%.2f" % avghumid)
 
@@ -128,14 +123,28 @@ def plot(i):
     fd.close()
    
  #os.remove('C:/Users/cordinge/Desktop/RaspberryPi_Sensor-master/plot.png')  
- fig.savefig('C:/Users/cordinge/Desktop/RaspberryPi_Sensor-master/plot.png', bbox_inches='tight', dpi=80)   
+ #fig.savefig('C:/Users/cordinge/Desktop/RaspberryPi_Sensor-master/plot.png', bbox_inches='tight', dpi=80)   
  
  
  fig.tight_layout()  
- os.system(CMD)
+ 
 
-ani = animation.FuncAnimation(fig, plot, interval=10000)
-#fig.set_visible(not fig.get_visible())
+ os.system(CMD)
+''' 
+ if (response != 1):
+      print('hi')
+      os.system(CMD)
+     
+ if response != 1:
+      print("Raspberry is currently offline... Exiting Program...")
+      time.sleep(5)
+      exit()
+'''
+ 
+
+
+ani = animation.FuncAnimation(fig, plot, interval=5000)
+
 plt.plot()
 
     
